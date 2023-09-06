b007d8a8b5429597430553e52d82556b82aa5ad2
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-09-06 13:39:02 +0800

    Refactor abstract node

--- Team30/Code30/src/spa/src/source_processor/node/ANode.cpp
+++ Team30/Code30/src/spa/src/source_processor/node/ANode.cpp
@@ -1,9 +1,1 @@
 #include "ANode.h"
-
-Saving_Account sav_acc {};
-Saving_Account *p_sav_acc = new Saving_Account();
-
-sav_account.deposit(1000);
-p_sav_account->withdraw(200);
-
-delete p_save_acc;
