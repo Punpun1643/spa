cd5b228799d2d512cf3b96c48855013b2d71127a
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-09-07 01:44:09 +0800

    Visitor pattern for call node

--- Team30/Code30/src/spa/src/source_processor/design_extractor/IDesignExtractor.h
+++ Team30/Code30/src/spa/src/source_processor/design_extractor/IDesignExtractor.h
@@ -5,6 +5,9 @@
 #include "../node/ProgramNode.h"
 #include "../node/ProcedureNode.h"
 #include "../node/StmtLstNode.h"
+#include "../node/stmt_node/CallNode.h"
+#include "../node/stmt_node/PrintNode.h"
+#include "../node/stmt_node/ReadNode.h"
 
 class IDesignExtractor {
 public:
@@ -13,6 +16,12 @@ public:
     virtual void extractFromProcedure(std::shared_ptr <ProcedureNode> node) = 0;
 
     virtual void extractFromStmtLst(std::shared_ptr <StmtLstNode> node) = 0;
+
+    virtual void extractFromCall(std::shared_ptr <CallNode> node) = 0;
+
+    virtual void extractFromPrint(std::shared_ptr <PrintNode> node) = 0;
+
+    virtual void extractFromRead(std::shared_ptr <ReadNode> node) = 0;
 
     virtual ~IDesignExtractor() = default;
 };
--- Team30/Code30/src/spa/src/source_processor/node/stmt_node/CallNode.h
+++ Team30/Code30/src/spa/src/source_processor/node/stmt_node/CallNode.h
@@ -9,6 +9,8 @@ class CallNode : public StmtNode {
 public:
     explicit CallNode(int stmtIndex, StmtType stmtType, std::string procName);
 
+    void accept(IDesignExtractor &designExtractor) override;
+
     ~CallNode() = default;
 
 private:
