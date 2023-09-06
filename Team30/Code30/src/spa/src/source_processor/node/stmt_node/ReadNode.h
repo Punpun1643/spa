f33c3169ddd17b73dc87bb250affb6f013c31287
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-09-07 01:47:46 +0800

    Visitor pattern for read node

--- Team30/Code30/src/spa/src/source_processor/node/stmt_node/ReadNode.h
+++ Team30/Code30/src/spa/src/source_processor/node/stmt_node/ReadNode.h
@@ -9,6 +9,8 @@ class ReadNode : public StmtNode {
 public:
     explicit ReadNode(int stmtIndex, StmtType stmtType, std::string varName);
 
+    void accept(IDesignExtractor &designExtractor) override;
+
     ~ReadNode() = default;
 
 private:
