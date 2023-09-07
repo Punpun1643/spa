9d05d2c95e0f469461376435d0216aec4e4c1f43
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-09-07 18:39:42 +0800

    Setup abstract parser

--- Team30/Code30/src/spa/src/shared/parser/AParser.cpp
+++ Team30/Code30/src/spa/src/shared/parser/AParser.cpp
@@ -7,3 +7,13 @@ Token AParser::nextToken() {
         return tokens[currTokenIndex++];
     }
 };
+
+Token AParser::peekToken() {
+    if (currTokenIndex + 1 < tokens.size()) {
+        return tokens[currTokenIndex + 1];
+    }
+}
+
+Token AParser::getCurrToken() {
+    return tokens[currTokenIndex];
+}
