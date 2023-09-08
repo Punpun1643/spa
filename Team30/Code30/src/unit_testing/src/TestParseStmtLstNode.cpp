b137a41e7e37017db89043535ed39ff53face7a4
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-09-08 18:22:40 +0800

    Pass test case for one print stmt

--- Team30/Code30/src/unit_testing/src/TestParseStmtLstNode.cpp
+++ Team30/Code30/src/unit_testing/src/TestParseStmtLstNode.cpp
@@ -4,6 +4,7 @@
 #include "../../spa/src/shared/tokenizer/token/SpecialCharToken.h"
 #include "../../spa/src/shared/tokenizer/token/EofToken.h"
 #include "../../spa/src/shared/tokenizer/token/WordToken.h"
+#include "../../spa/src/source_processor/node/stmt_node/PrintNode.h"
 
 TEST_CASE("Parser parseStmtLst") {
     std::vector<std::shared_ptr<Token>> tokens;
@@ -13,5 +14,21 @@ TEST_CASE("Parser parseStmtLst") {
 
         SpParser parser = SpParser(tokens);
         REQUIRE(parser.parseStmtLst()->getChildren().empty());
+    }
+
+    SECTION("StmtLst with one statement return one statement node") {
+        tokens.push_back(std::static_pointer_cast<Token>(std::make_shared<WordToken>("print")));
+        tokens.push_back(std::static_pointer_cast<Token>(std::make_shared<WordToken>("variable")));
+        tokens.push_back(std::static_pointer_cast<Token>(std::make_shared<SpecialCharToken>(";")));
+        tokens.push_back(std::static_pointer_cast<Token>(std::make_shared<SpecialCharToken>("}")));
+
+        SpParser parser = SpParser(tokens);
+        auto stmtLstNode = parser.parseStmtLst();
+        auto printNode = std::dynamic_pointer_cast<PrintNode>(stmtLstNode->getChildren().at(0));
+
+        REQUIRE(stmtLstNode != nullptr);
+        REQUIRE(stmtLstNode->getChildren().size() == 1);
+        REQUIRE(printNode != nullptr);
+        REQUIRE(printNode->varName == "variable");
     }
 }
