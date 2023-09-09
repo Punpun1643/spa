aba1cc57a38a99885f3d45afca32c06dcc9f2485
limweiliang <limweiliang4@gmail.com>
2023-09-09 22:23:18 +0800

    Add interface for PKB

---
+++ Team30/Code30/src/spa/src/program_knowledge_base/PkbApi.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+#include <optional>
+#include "../query_processing_system/common/EntityType.h"
+
+class PkbApi {
+ public:
+  virtual std::unique_ptr<std::vector<std::string>> getEntitiesWithType(EntityType type) = 0;
+  virtual bool isFollows(int s1_line_num, int s2_line_num) = 0;
+  virtual std::optional<std::pair<int, int>> getFollows(int s1_line_num, EntityType s2_type) = 0;
+  virtual std::optional<std::pair<int, int>> getFollows(EntityType s1_type, int s2_line_num) = 0;
+  virtual std::unique_ptr<std::vector<std::pair<int, int>>> getFollows(EntityType s1_type, EntityType s2_type) = 0;
+};
