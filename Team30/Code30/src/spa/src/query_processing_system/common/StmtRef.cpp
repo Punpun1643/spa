64d182ea5939e2fc5a2379ac3a18ce668575f0aa
limweiliang <limweiliang4@gmail.com>
2023-09-09 13:56:13 +0800

    Add functionality for StmtRef

--- Team30/Code30/src/spa/src/query_processing_system/common/StmtRef.cpp
+++ Team30/Code30/src/spa/src/query_processing_system/common/StmtRef.cpp
@@ -3,8 +3,34 @@
 #include "EntityType.h"
 
 StmtRef::StmtRef() {
-  /* Initializes a wild statement reference */
+  ref_type = WILD;
 }
 
-StmtRef::StmtRef(std::shared_ptr<PqlDeclaration> declaration) {}
-StmtRef::StmtRef(int stmt_num) {}
+StmtRef::StmtRef(std::shared_ptr<PqlDeclaration> declaration) {
+  ref_type = DECLARATION;
+  this->declaration = declaration;
+}
+
+StmtRef::StmtRef(int stmt_num) {
+  ref_type = NUMBER;
+  this->stmt_num = stmt_num;
+}
+
+StmtRefType StmtRef::getStmtRefType() {
+  return ref_type;
+}
+
+int StmtRef::getStmtNum() {
+  if (ref_type == NUMBER) {
+      return stmt_num;
+  } else {
+      throw std::runtime_error("StmtRef does not contain a statement number.");
+  }
+}
+std::shared_ptr<PqlDeclaration> StmtRef::getDeclaration() {
+  if (ref_type == DECLARATION) {
+      return declaration;
+  } else {
+      throw std::runtime_error("StmtRef does not contain a PQL declaration.");
+  }
+}
--- Team30/Code30/src/spa/src/query_processing_system/common/StmtRef.h
+++ Team30/Code30/src/spa/src/query_processing_system/common/StmtRef.h
@@ -4,13 +4,29 @@
 #include "EntityType.h"
 #include "PqlDeclaration.h"
 
+enum StmtRefType {
+  WILD,
+  NUMBER,
+  DECLARATION
+};
+
+
 class StmtRef {
  private:
+  StmtRefType ref_type;
+  int stmt_num;
+  std::shared_ptr<PqlDeclaration> declaration;
 
-public:
+ public:
   StmtRef();
 
   explicit StmtRef(int stmt_num);
 
   explicit StmtRef(std::shared_ptr<PqlDeclaration> stmt_type);
+
+  StmtRefType getStmtRefType();
+
+  int getStmtNum();
+
+  std::shared_ptr<PqlDeclaration> getDeclaration();
 };
