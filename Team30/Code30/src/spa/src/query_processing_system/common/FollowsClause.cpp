437c004e45066d7843e7017ad4b62baf8aa52cd7
limweiliang <limweiliang4@gmail.com>
2023-09-09 13:56:40 +0800

    Change initialization for FollowsClause

--- Team30/Code30/src/spa/src/query_processing_system/common/FollowsClause.cpp
+++ Team30/Code30/src/spa/src/query_processing_system/common/FollowsClause.cpp
@@ -1,8 +1,8 @@
 #include "FollowsClause.h"
 
-FollowsClause::FollowsClause(std::unique_ptr<StmtRef> arg1, std::unique_ptr<StmtRef> arg2) {
-  this->arg1 = std::move(arg1);
-  this->arg2 = std::move(arg2);
+FollowsClause::FollowsClause(std::unique_ptr<StmtRef> arg1,
+                             std::unique_ptr<StmtRef> arg2):
+arg1(std::move(arg1)), arg2(std::move(arg2)){
   // verify inputs are semantically valid.
 }
 
