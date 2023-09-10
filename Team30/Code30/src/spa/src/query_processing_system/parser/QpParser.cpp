04e81df0a145c97d307affaa72b6efda8bfb1172
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2023-09-11 01:51:18 +0800

    add select clause parsing for QpParser

--- Team30/Code30/src/spa/src/query_processing_system/parser/QpParser.cpp
+++ Team30/Code30/src/spa/src/query_processing_system/parser/QpParser.cpp
@@ -1,1 +1,64 @@
 #include "QpParser.h"
+#include "../../shared/tokenizer/token/SpecialCharToken.h"
+#include "../../shared/tokenizer/token/Token.h"
+#include "../../shared/tokenizer/token/TokenType.h"
+#include "../common/SelectClause.h"
+#include <map>
+
+bool isWordToken(std::shared_ptr<Token> token) {
+  if (token->getTokenType() != TokenType::WORD_TOKEN) {
+    return false;
+  }
+  return true;
+}
+
+bool isSpecialCharToken(std::shared_ptr<Token> token) {
+  if (token->getTokenType() != TokenType::SPECIAL_CHAR_TOKEN) {
+    return false;
+  }
+  return true;
+}
+
+std::shared_ptr<ParsedQuery> parse() {
+  map<std::string, std::shared_ptr<PqlDeclaration>> declarations;
+
+  // get declarations
+  while (getCurrToken()->getTokenVal() != "select") {
+    std::shared_ptr<Token> currToken = getCurrToken();
+    std::string tokenValue = currToken->getTokenVal();
+    if (isWordToken(currToken) && tokenValue == "stmt") {
+      nextToken();
+      std::shared_ptr<Token> nameToken = getCurrToken();
+      std::shared_ptr<PqlDeclaration> declaration = new PqlDeclaration(
+          new std::string(nameToken->getTokenVal()), EntityType::STMT);
+      declarations.insert(make_pair(nameToken->getTokenValue(), declaration));
+      nextToken();
+    }
+    std::shared_ptr<Token> semicolonToken = getCurrToken();
+    std::string tokenValue = semicolonToken->getTokenVal();
+    if (isSpecialCharToken(currToken) && tokenValue) {
+      nextToken();
+      continue;
+    } else {
+      throw std::invalid_argument("Invalid declaration");
+    }
+  }
+
+  ParsedQuery parsedQuery;
+  // select
+  while (getCurrToken()->getTokenType() != TokenType::EOF_TOKEN) {
+    std::shared_ptr<Token> currToken = getCurrToken();
+    EntityType selectionEntityType;
+    nextToken();
+    std::shared_ptr<Token> entityNameToken = getCurrToken();
+    std::shared_ptr<PqlDeclaration> declaration =
+        map[entityNametoken->getTokenVal()];
+    std::shared_ptr<SelectClause> selectClause =
+        new SelectClause(declaration) parsedQuery.selectClause = selectClause;
+    nextToken();
+    // such that clause stuff follows here. implement follows first. abstract
+    // each clause algo into a function?
+  }
+
+  return parsedQuery;
+}
--- Team30/Code30/src/spa/src/query_processing_system/parser/QpParser.h
+++ Team30/Code30/src/spa/src/query_processing_system/parser/QpParser.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "../common/FollowsClause.h"
+#include "../common/SelectClause.h"
+#include "../common/SuchThatClause.h"
+#include "../../shared/parser/AParser.h"
+#include "../../shared/tokenizer/token/Token.h"
+
+struct ParsedQuery {
+  SelectClause selectClause;
+  SuchThatClause suchThatClause;
+}
+
+class QpParser : public AParser {
+  public:
+    QpParser(std::vector <std::shared_ptr<Token>> tokens);
+
+    std::shared_ptr <ParsedQuery> parse() override;
+}
