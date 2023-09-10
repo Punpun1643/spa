fb29b4eefc961f3b762f15b18c1378039a780d96
limweiliang <limweiliang4@gmail.com>
2023-09-10 20:31:41 +0800

    Reformat code

--- Team30/Code30/src/spa/src/program_knowledge_base/PkbApi.cpp
+++ Team30/Code30/src/spa/src/program_knowledge_base/PkbApi.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Lim Wei Liang on 9/9/23.
-//
-
 #include "PkbApi.h"
 
 PkbApi::~PkbApi() = default;
--- Team30/Code30/src/spa/src/program_knowledge_base/PkbApi.h
+++ Team30/Code30/src/spa/src/program_knowledge_base/PkbApi.h
@@ -1,16 +1,21 @@
 #pragma once
-#include <string>
-#include <vector>
 #include <memory>
-#include <utility>
 #include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
 #include "../query_processing_system/common/EntityType.h"
 
 class PkbApi {
  public:
-  virtual std::unique_ptr<std::vector<std::string>> getEntitiesWithType(EntityType type) = 0;
-  virtual std::optional<std::pair<int, int>> getFollows(int s1_line_num, EntityType s2_type) = 0;
-  virtual std::optional<std::pair<int, int>> getFollows(EntityType s1_type, int s2_line_num) = 0;
-  virtual std::unique_ptr<std::vector<std::pair<int, int>>> getFollows(EntityType s1_type, EntityType s2_type) = 0;
-  virtual ~PkbApi(); // so that the subclass destructors will be called
+  virtual std::unique_ptr<std::vector<std::string>> getEntitiesWithType(
+      EntityType type) = 0;
+  virtual std::optional<std::pair<int, int>> getFollows(int s1_line_num,
+                                                        EntityType s2_type) = 0;
+  virtual std::optional<std::pair<int, int>> getFollows(EntityType s1_type,
+                                                        int s2_line_num) = 0;
+  virtual std::unique_ptr<std::vector<std::pair<int, int>>> getFollows(
+      EntityType s1_type, EntityType s2_type) = 0;
+  virtual ~PkbApi();  // so that the subclass destructors will be called
 };
