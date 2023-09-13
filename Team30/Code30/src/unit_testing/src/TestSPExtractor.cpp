f1d665f7ac0dfcac904d83e1a0dbeb590062c2fe
ryanongra <ryanongra@gmail.com>
2023-09-13 21:42:08 +0800

    Update TestSPExtractor.cpp

--- Team30/Code30/src/unit_testing/src/TestSPExtractor.cpp
+++ Team30/Code30/src/unit_testing/src/TestSPExtractor.cpp
@@ -1,48 +1,97 @@
-#include "../../spa/src/program_knowledge_base/PkbApi.h"
-
-#include "catch.hpp"
-#include <source_processor/node/stmt_node/PrintNode.h>
-#include <source_processor/node/StmtLstNode.h>
+#include <source_processor/design_extractor/IExtractionController.h>
 #include <source_processor/node/ProcedureNode.h>
 #include <source_processor/node/ProgramNode.h>
-#include <source_processor/design_extractor/IExtractionController.h>
+#include <source_processor/node/StmtLstNode.h>
+#include <source_processor/node/stmt_node/PrintNode.h>
+
+#include <iostream>
 #include <source_processor/design_extractor/ExtractionController.cpp>
 
-class PkbStub : public PkbApi {
-  static bool insertFollows(std::shared_ptr<StmtNode> stmt1,
-      std::shared_ptr<StmtNode> stmt2) {
-    fCounter_insertFollows++;
-  }
+#include "../../spa/src/program_knowledge_base/PkbApi.h"
+#include "catch.hpp"
 
-  static int getFCounter_insertFollows() { 
-      return fCounter_insertFollows;
-  }
+//class PkbStub : public PkbApi {
+//  bool insertFollows(std::shared_ptr<StmtNode> stmt1,
+//                     std::shared_ptr<StmtNode> stmt2) {
+//    fCounter_insertFollows++;
+//    std::cout << "InsertFollows called";
+//    return true;
+//  }
+//
+//  int getFCounter_insertFollows() { return fCounter_insertFollows; }
+//
+//  std::unique_ptr<std::vector<std::string>> getEntitiesWithType(
+//      EntityType type) {
+//    return NULL;
+//  }
+//  std::optional<std::pair<int, int>> getFollows(int s1_line_num,
+//                                                EntityType s2_type) {
+//    return std::make_optional<std::pair<int, int>>();
+//  }
+//  std::optional<std::pair<int, int>> getFollows(EntityType s1_type,
+//                                                int s2_line_num) {
+//    return std::make_optional<std::pair<int, int>>();
+//  };
+//  virtual std::unique_ptr<std::vector<std::pair<int, int>>> getFollows(
+//      EntityType s1_type, EntityType s2_type) {
+//    return NULL;
+//  }
+//
+// public:
+//  int fCounter_insertFollows;
+//};
 
+class PkbApiStub : public PkbApi {
  public:
-  static int fCounter_insertFollows;
+  int insertFollowsCallCount;
+  PkbApiStub() : insertFollowsCallCount(0) {}
 
+  bool insertFollows(std::shared_ptr<StmtNode> stmt1,
+                     std::shared_ptr<StmtNode> stmt2) {
+    ++insertFollowsCallCount;
+    return true;
+  }
+
+  std::unique_ptr<std::vector<std::string>> getEntitiesWithType(
+      EntityType type) override {
+    std::unique_ptr<std::vector<std::string>> output =
+        std::make_unique<std::vector<std::string>>();
+    return output;
+  }
+
+  std::optional<std::pair<int, int>> getFollows(int s1_line_num,
+                                                EntityType s2_type) override {
+    return std::nullopt;
+  }
+
+  std::optional<std::pair<int, int>> getFollows(EntityType s1_type,
+                                                int s2_line_num) override {
+    return std::nullopt;
+  }
+
+  std::unique_ptr<std::vector<std::pair<int, int>>> getFollows(
+      EntityType s1_type, EntityType s2_type) override {
+    return std::make_unique<std::vector<std::pair<int, int>>>();
+  }
 };
 
 TEST_CASE("Extract Follows") {
-  std::shared_ptr<PrintNode> p1 = std::make_shared<PrintNode>(
-      new PrintNode(1, StmtType::PRINT_STMT, "var1"));
-  std::shared_ptr<PrintNode> p2 = std::make_shared<PrintNode>(
-      new PrintNode(2, StmtType::PRINT_STMT, "var1"));
+  std::string varName = "var";
+  PrintNode p1 = PrintNode(1, StmtType::PRINT_STMT, "varName");
+  PrintNode p2 = PrintNode(2, StmtType::PRINT_STMT, "varName");
   std::vector<std::shared_ptr<StmtNode>> stmts;
-  stmts.push_back(p1);
-  stmts.push_back(p2);
-  std::shared_ptr<StmtLstNode> stl = std::make_shared<StmtLstNode>(
-      new StmtLstNode(stmts));
-  std::shared_ptr<ProcedureNode> proc =
-      std::make_shared<ProcedureNode>(new ProcedureNode("proc", stl));
+  stmts.push_back(std::make_shared<PrintNode>(p1));
+  stmts.push_back(std::make_shared<PrintNode>(p2));
+  StmtLstNode stl = StmtLstNode(stmts);
+  ProcedureNode proc =
+      ProcedureNode("proc", std::make_shared<StmtLstNode>(stl));
   std::vector<std::shared_ptr<ProcedureNode>> procs;
-  procs.push_back(proc);
-  std::shared_ptr<ProgramNode> prog =
-      std::make_shared<ProgramNode>(new ProgramNode(procs));
+  procs.push_back(std::make_shared<ProcedureNode>(proc));
+  ProgramNode prog = ProgramNode(procs);
 
-  ExtractionController ec = ExtractionController();
-  ec.executeExtract(prog);
-  //int called = PkbStub::getFCounter_insertFollows();
-  int calls = PkbStub::fCounter_insertFollows;
-  REQUIRE(calls == 1);
+  PkbApiStub pkb = PkbApiStub();
+  ExtractionController ec = ExtractionController(pkb);
+  ec.executeProgramExtraction(std::make_shared<ProgramNode>(prog));
+  //ec.executeStmtLstExtraction(std::make_shared<StmtLstNode>(stl));
+  REQUIRE(pkb.insertFollowsCallCount == 1);
 }
