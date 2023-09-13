171be99acb90222ef54546a0e4ad7305fa80342f
limweiliang <limweiliang4@gmail.com>
2023-09-13 17:31:42 +0800

    Add comments to PkbApi

--- Team30/Code30/src/spa/src/program_knowledge_base/PkbApi.h
+++ Team30/Code30/src/spa/src/program_knowledge_base/PkbApi.h
@@ -19,27 +19,36 @@ class PkbApi {
       EntityType type) = 0;
 
   // 0 Declarations
+  // Relation(value, value) e.g. Follows (int, int)
   virtual bool isRelationTrue(std::string value_1, std::string value_2,
                               RelationType rel_type) = 0;
+  // Relation(value, wild) e.g. Follows (int, _)
   virtual bool isRelationTrueGivenFirstValue(std::string value,
                                              RelationType rel_type) = 0;
+  // Relation(wild, value) e.g. Follows(_, int)
   virtual bool isRelationTrueGivenSecondValue(std::string value,
                                               RelationType rel_type) = 0;
+  // Relation(wild, wild) e.g. Follows(_, _)
   virtual bool isRelationTrueForAny(RelationType relation_type) = 0;
 
   // 1 Declarations
+  // Relation(declaration, wild) e.g. Follows(s, _)
   virtual std::unique_ptr<std::vector<std::string>>
   getRelationValuesGivenFirstType(EntityType entity_type,
                                   RelationType rel_type) = 0;
+  // Relation(wild, declaration) e.g. Follows(_, s)
   virtual std::unique_ptr<std::vector<std::string>>
   getRelationValuesGivenSecondType(EntityType entity_type,
                                    RelationType rel_type) = 0;
+  // Relation(declaration, value) e.g. Follows(s, 5)
   virtual std::unique_ptr<std::vector<std::string>> getRelationValues(
       EntityType entity_type, std::string value, RelationType rel_type) = 0;
+  // Relation(value, declaration) e.g. Follows(5, s)
   virtual std::unique_ptr<std::vector<std::string>> getRelationValues(
       std::string value, EntityType entity_type, RelationType rel_type) = 0;
 
   // 2 Declarations
+  // Relation (declaration, declaration) e.g. Follows(s1, s2)
   virtual std::unique_ptr<std::vector<std::pair<std::string, std::string>>>
   getRelationValues(EntityType entity_type_1, EntityType entity_type_2,
                     RelationType rel_type) = 0;
