0b8db5a78d45232282adb2e63035cffbaee9110c
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-09-14 19:18:28 +0800

    Minor reformat

--- Team30/Code30/src/spa/src/source_processor/node/util_node/CondExprNode.cpp
+++ Team30/Code30/src/spa/src/source_processor/node/util_node/CondExprNode.cpp
@@ -4,6 +4,8 @@ CondExprNode::CondExprNode(std::unordered_set<std::string> variables,
                            std::unordered_set<int> constants)
     : variables(std::move(variables)), constants(std::move(constants)) {}
 
-std::unordered_set<std::string> CondExprNode::getVariables() const { return variables; }
+std::unordered_set<std::string> CondExprNode::getVariables() const {
+  return variables;
+}
 
 std::unordered_set<int> CondExprNode::getConstants() const { return constants; }
--- Team30/Code30/src/spa/src/source_processor/node/util_node/CondExprNode.h
+++ Team30/Code30/src/spa/src/source_processor/node/util_node/CondExprNode.h
@@ -1,18 +1,19 @@
 #pragma once
 
-#include <unordered_set>
 #include <string>
+#include <unordered_set>
 
 class CondExprNode {
  public:
-  explicit CondExprNode(std::unordered_set<std::string> variables, std::unordered_set<int> constants);
+  explicit CondExprNode(std::unordered_set<std::string> variables,
+                        std::unordered_set<int> constants);
 
-        std::unordered_set<std::string> getVariables() const;
-        std::unordered_set<int> getConstants() const;
+  std::unordered_set<std::string> getVariables() const;
+  std::unordered_set<int> getConstants() const;
 
   ~CondExprNode() = default;
 
  private:
-        std::unordered_set<std::string> variables;
-        std::unordered_set<int> constants;
+  std::unordered_set<std::string> variables;
+  std::unordered_set<int> constants;
 };
