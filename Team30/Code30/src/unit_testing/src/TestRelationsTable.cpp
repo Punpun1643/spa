8d27b13e199469e785c3e5157af4dd5433c77c9e
EssWhyy <sventang1@gmail.com>
2023-09-15 12:32:42 +0800

    Add Unit Tests for Uses & Modifies

--- Team30/Code30/src/unit_testing/src/TestRelationsTable.cpp
+++ Team30/Code30/src/unit_testing/src/TestRelationsTable.cpp
@@ -40,5 +40,32 @@ TEST_CASE("Parents Star & Follows Star (UFDSTable)") {
   REQUIRE(ps.isRelated("1", "7"));
   // non symmetric + transitive
   REQUIRE(ps.isRelated("7", "1") == false);
-
 }
+
+ TEST_CASE("Uses & Modifies (Dictionary Table)") {
+    DictionaryTable p = DictionaryTable();
+
+    p.insert("2", "foo");
+    p.insert("2", "bar");
+    p.insert("5", "foo");
+    p.insert("main", "bar");
+
+    // added relation
+    REQUIRE(ps.isRelated("2", "foo"));
+    // non added relation
+    REQUIRE(ps.isRelated("3", "flan") == false);
+
+    // non reflexive
+    REQUIRE(ps.isRelated("2", "2") == false);
+    // non symmetric
+    REQUIRE(ps.isRelated("foo", "5") == false);
+
+    REQUIRE(ps.isRelated("main", "2") == false);
+    
+    //TODO: Case where procedure contains a statement that has a known Relation
+    //But no way to know if procedure contains statement? 
+    //Eg. "main" proc contains stmt 2 and Uses/Modifies(2, foo) is true
+    //REQUIRE(ps.isRelated("main", "foo")); 
+    
+
+ }
