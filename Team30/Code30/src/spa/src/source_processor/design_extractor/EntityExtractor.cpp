39acd1de926c69ee95dc0718d51fad7c2eeba441
ryanongra <ryanongra@gmail.com>
2023-09-15 23:52:55 +0800

    Implement entity extraction

--- Team30/Code30/src/spa/src/source_processor/design_extractor/EntityExtractor.cpp
+++ Team30/Code30/src/spa/src/source_processor/design_extractor/EntityExtractor.cpp
@@ -8,7 +8,7 @@ void EntityExtractor::extractFromProgram(std::shared_ptr<ProgramNode> node) {
 
 void EntityExtractor::extractFromProcedure(
     std::shared_ptr<ProcedureNode> node) {
-  // pkb.insertEntity(EntityType::PROCEDURE, node->getProcedureName);
+   pkb.insertEntity(EntityType::PROCEDURE, node->getProcedureName());
 }
 
 void EntityExtractor::extractFromStmtLst(std::shared_ptr<StmtLstNode> node) {
@@ -16,19 +16,23 @@ void EntityExtractor::extractFromStmtLst(std::shared_ptr<StmtLstNode> node) {
 }
 
 void EntityExtractor::extractFromCall(std::shared_ptr<CallNode> node) {
-  // pkb.insertEntity(EntityType::CALL, std::to_string(node->getStmtIndex()));
+   pkb.insertEntity(EntityType::CALL, std::to_string(node->getStmtIndex()));
 }
 
 void EntityExtractor::extractFromPrint(std::shared_ptr<PrintNode> node) {
-  // pkb.insertEntity(EntityType::PRINT, std::to_string(node->getStmtIndex()));
+   pkb.insertEntity(EntityType::PRINT, std::to_string(node->getStmtIndex()));
 }
 
 void EntityExtractor::extractFromRead(std::shared_ptr<ReadNode> node) {
-  // pkb.insertEntity(EntityType::READ, std::to_string(node->getStmtIndex()));
+   pkb.insertEntity(EntityType::READ, std::to_string(node->getStmtIndex()));
 }
 
-void EntityExtractor::extractFromWhile(std::shared_ptr<WhileNode> node) {}
+void EntityExtractor::extractFromWhile(std::shared_ptr<WhileNode> node) {
+   pkb.insertEntity(EntityType::WHILE, std::to_string(node->getStmtIndex()));
+}
 
-void EntityExtractor::extractFromIf(std::shared_ptr<IfNode> node) {}
+void EntityExtractor::extractFromIf(std::shared_ptr<IfNode> node) {
+   pkb.insertEntity(EntityType::IF, std::to_string(node->getStmtIndex()));
+}
 
 // void EntityExtractor::extractFromAssign(std::shared_ptr<AssignNode> node) {}
