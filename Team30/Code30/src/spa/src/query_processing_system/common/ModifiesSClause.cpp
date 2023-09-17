37779400361b2d69cd5e33f389b1c1f3be6075f6
limweiliang <limweiliang4@gmail.com>
2023-09-17 13:51:36 +0800

    Fix typo

--- Team30/Code30/src/spa/src/query_processing_system/common/ModifiesSClause.cpp
+++ Team30/Code30/src/spa/src/query_processing_system/common/ModifiesSClause.cpp
@@ -32,13 +32,13 @@ ModifiesSClause::ModifiesSClause(std::unique_ptr<StmtRef> arg1,
 
 std::unique_ptr<ClauseResult> ModifiesSClause::evaluateWildDeclaration(
     PkbApi& pkb) {
-  throw InvalidSemanticsException("1st argument of Uses cannot be a wildcard");
+  throw InvalidSemanticsException("1st argument of Modifies cannot be a wildcard");
 }
 
 std::unique_ptr<ClauseResult> ModifiesSClause::evaluateWildWild(PkbApi& pkb) {
-  throw InvalidSemanticsException("1st argument of Uses cannot be a wildcard");
+  throw InvalidSemanticsException("1st argument of Modifies cannot be a wildcard");
 }
 
 std::unique_ptr<ClauseResult> ModifiesSClause::evaluateWildValue(PkbApi& pkb) {
-  throw InvalidSemanticsException("1st argument of Uses cannot be a wildcard");
+  throw InvalidSemanticsException("1st argument of Modifies cannot be a wildcard");
 }
