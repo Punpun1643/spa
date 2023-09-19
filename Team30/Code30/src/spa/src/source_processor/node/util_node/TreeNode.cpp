e4a360d7f2c23e892181b6442090402fa2a60d0d
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-09-19 15:50:07 +0800

    Declare TreeNode structure

--- Team30/Code30/src/spa/src/source_processor/node/util_node/TreeNode.cpp
+++ Team30/Code30/src/spa/src/source_processor/node/util_node/TreeNode.cpp
@@ -1,1 +1,2 @@
 #include "TreeNode.h"
+
--- Team30/Code30/src/spa/src/source_processor/node/util_node/TreeNode.h
+++ Team30/Code30/src/spa/src/source_processor/node/util_node/TreeNode.h
@@ -1,7 +1,23 @@
 #pragma once
 
+#include <memory>
+#include <string>
+
 class TreeNode {
  public:
-  TreeNode();
+  TreeNode(std::string val, std::shared_ptr<TreeNode> leftSubTree,
+           std::shared_ptr<TreeNode> rightSubTree);
+
+  std::string getVal() const;
+
+  std::shared_ptr<TreeNode> getLeftSubTree() const;
+
+  std::shared_ptr<TreeNode> getRightSubTree() const;
+
   ~TreeNode() = default;
+
+ private:
+  std::string val;
+  std::shared_ptr<TreeNode> leftSubTree;
+  std::shared_ptr<TreeNode> rightSubTree;
 };
