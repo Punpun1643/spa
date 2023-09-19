e1605b382f80ae609fec33f82c46506e34a6b20f
ryanongra <ryanongra@gmail.com>
2023-09-19 22:51:07 +0800

    Resolve conflicts

--- Team30/Code30/src/spa/src/source_processor/design_extractor/UsesExtractor.cpp
+++ Team30/Code30/src/spa/src/source_processor/design_extractor/UsesExtractor.cpp
@@ -46,7 +46,7 @@ void UsesExtractor::extractFromIf(std::shared_ptr<IfNode> node) {
   usesActors.push_back(std::to_string(node->getStmtIndex()));
 }
 
-// void UsesExtractor::extractFromAssign(std::shared_ptr<AssignNode> node) {}
+void UsesExtractor::extractFromAssign(std::shared_ptr<AssignNode> node) {}
 
 void UsesExtractor::popUsesActors() {
   if (!usesActors.empty()) {
