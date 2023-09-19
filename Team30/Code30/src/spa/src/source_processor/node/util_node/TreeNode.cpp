8af5c3cb4893072d23010cb267653dd864682a7b
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-09-19 15:53:07 +0800

    Concrete TreeNode

--- Team30/Code30/src/spa/src/source_processor/node/util_node/TreeNode.cpp
+++ Team30/Code30/src/spa/src/source_processor/node/util_node/TreeNode.cpp
@@ -1,2 +1,15 @@
 #include "TreeNode.h"
 
+TreeNode::TreeNode(std::string val, std::shared_ptr<TreeNode> leftSubTree,
+                   std::shared_ptr<TreeNode> rightSubTree)
+    : val(val), leftSubTree(leftSubTree), rightSubTree(rightSubTree) {}
+
+std::string TreeNode::getVal() const { return val; }
+
+std::shared_ptr<TreeNode> TreeNode::getLeftSubTree() const {
+  return leftSubTree;
+}
+
+std::shared_ptr<TreeNode> TreeNode::getRightSubTree() const {
+  return rightSubTree;
+}
