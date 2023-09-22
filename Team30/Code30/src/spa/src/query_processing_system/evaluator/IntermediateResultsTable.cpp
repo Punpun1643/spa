a020b2439a29b9220d90ab4b628b482d90e5029a
limweiliang <limweiliang4@gmail.com>
2023-09-22 16:20:59 +0800

    Update IntermediateResultsTable skeleton

--- Team30/Code30/src/spa/src/query_processing_system/evaluator/IntermediateResultsTable.cpp
+++ Team30/Code30/src/spa/src/query_processing_system/evaluator/IntermediateResultsTable.cpp
@@ -3,38 +3,6 @@
 
 IntermediateResultsTable::IntermediateResultsTable() = default;
 
-void IntermediateResultsTable::addBooleanClauseResult(bool result) {
-  if (!result) {
-    has_no_results = true;
-  } // otherwise, do nothing
-}
-
-void IntermediateResultsTable::addSingleDeclaration(const PqlDeclaration & d, std::unique_ptr<std::vector<std::string>> values) {
-  // Declaration doesn't exist
-  if (value_map.count(d) == 0) {
-    value_map[d] = *values; // easy case
-  }
-  // Declaration already exists
-  auto existing_values = value_map[d];
-  auto indices_to_keep = ArrayUtility::getIndicesToKeep(existing_values, *values);
-  if (indices_to_keep->empty()) {
-    has_no_results = true;
-    return;
-  }
-  // filter this declaration
-  value_map[d] = *(ArrayUtility::getArrSubsetByIndices(value_map[d], *indices_to_keep));
-  // filter all connected declarations, if any
-  if (linked_declarations.count(d) == 1) {
-    for (const auto& linked_d: linked_declarations[d]) {
-      value_map[linked_d] = *(ArrayUtility::getArrSubsetByIndices(value_map[linked_d], *indices_to_keep));
-    }
-  }
-}
-
-void IntermediateResultsTable::addPairedDeclarations(const PqlDeclaration & d1, const PqlDeclaration & d2, std::unique_ptr<std::vector<std::string>> d1_values, std::unique_ptr<std::vector<std::string>> d2_values) {
-
-}
-
 void IntermediateResultsTable::addClauseResult(const ClauseResult& clause_result) {
   if (has_no_results) {
     return; // don't bother, stuck at false already
@@ -47,12 +15,19 @@ void IntermediateResultsTable::addClauseResult(const ClauseResult& clause_result
       break;
     }
     case (1): {
-//      auto declaration = clause_result.getDeclarations()->front();
-//      auto values = clause_result.getValues(declaration);
-//      addSingleDeclaration(declaration, std::move(values));
+      auto declaration = clause_result.getDeclarations().front();
+      auto values = clause_result.getValues(declaration);
+      addSingleDeclaration(declaration, *values);
       break;
     }
     case (2): {
+      auto declarations = clause_result.getDeclarations();
+      assert(declarations.size() == 2);
+      auto d1 = declarations[0];
+      auto d2 = declarations[1];
+      auto d1_values = clause_result.getValues(d1);
+      auto d2_values = clause_result.getValues(d2);
+      addPairedDeclarations(d1, d2, *d1_values, *d2_values);
       break;
     }
     default: {
@@ -61,11 +36,103 @@ void IntermediateResultsTable::addClauseResult(const ClauseResult& clause_result
   }
 }
 
-std::unique_ptr<std::vector<std::string>> IntermediateResultsTable::getValuesGivenDeclaration(const PqlDeclaration& declaration){
-  assert(value_map.count(declaration) == 1); // must have been in select stmt
+bool IntermediateResultsTable::hasNoResults() const {
+  return has_no_results;
+}
 
+std::vector<std::string> IntermediateResultsTable::getValuesGivenDeclaration(const PqlDeclaration& declaration) {
   if (has_no_results) {
-    return std::make_unique<std::vector<std::string>>(); // empty values
+    return {};  // empty values
+  } else {
+    return getTableCol(declaration); // TODO
   }
-  return std::make_unique<std::vector<std::string>>(value_map[declaration]);
+}
+
+void IntermediateResultsTable::addBooleanClauseResult(bool result) {
+  if (!result) {
+    has_no_results = true;
+  } // otherwise, do nothing
+}
+
+void IntermediateResultsTable::addSingleDeclaration(const PqlDeclaration & d, const std::vector<std::string> &values) {
+  // Declaration doesn't exist
+  if (table_mapping.count(d) == 0) {
+    addNewTable(d, values);
+  } else {
+    int table_idx = table_mapping[d];
+    tables[table_idx].filter(d, values);
+    if (tables[table_idx].is_empty()) {
+      has_no_results = true;
+      return;
+    }
+  }
+}
+//  // Declaration already exists
+//  auto existing_values = value_map[d];
+//  auto indices_to_keep = ArrayUtility::getIndicesToKeep(existing_values, values);
+//  if (indices_to_keep->empty()) {
+//    has_no_results = true;
+//    return;
+//  }
+//  // filter this declaration
+//  value_map[d] = *(ArrayUtility::getArrSubsetByIndices(value_map[d], *indices_to_keep));
+//  // filter all connected declarations, if any
+//  if (linked_declarations.count(d) == 1) {
+//    for (const auto& linked_d: linked_declarations[d]) {
+//      value_map[linked_d] = *(ArrayUtility::getArrSubsetByIndices(value_map[linked_d], *indices_to_keep));
+//    }
+//  }
+
+
+
+//
+void IntermediateResultsTable::addPairedDeclarations(
+const PqlDeclaration & d1,
+const PqlDeclaration & d2,
+    const std::vector<std::string> & new_d1_values,
+    const std::vector<std::string> & new_d2_values) {
+  // Both declarations not in table
+  if (table_mapping.count(d1) == 0 && table_mapping.count(d2) == 0) {
+    addNewTable(d1, d2, new_d1_values, new_d2_values);
+
+  } else if (table_mapping.count(d1) == 1 && table_mapping.count(d2) == 0) {
+    RelationalTable new_table = RelationalTable({d1, d2}, {new_d1_values, new_d2_values});
+    int d1_table_idx = table_mapping[d1];
+    tables[d1_table_idx] = tables[d1_table_idx].join(new_table, d1);
+
+//    auto existing_d1_values = value_map[d1];
+//    // Mirrored
+//    auto new_d1_indices_to_keep = ArrayUtility::getIndicesToKeep(*new_d1_values, value_map[d1]);
+//    addSingleDeclaration(d1, std::move(new_d1_values));
+//    auto filtered_new_d2 = ArrayUtility::getArrSubsetByIndices(*new_d2_values, *new_d1_indices_to_keep);
+//    value_map[d2] = *filtered_new_d2;
+//    linked_declarations[]
+
+  } else if (table_mapping.count(d1) == 0 && table_mapping.count(d2) == 1) {
+    RelationalTable new_table = RelationalTable({d1, d2}, {new_d1_values, new_d2_values});
+    int d2_table_idx = table_mapping[d2];
+    tables[d2_table_idx] = tables[d2_table_idx].join(new_table, d2);
+  } else {
+    // case 1: d1 and d2 in same table already
+    if (table_mapping[d1] == table_mapping[d2]) {
+      RelationalTable new_table = RelationalTable({d1, d2}, {new_d1_values, new_d2_values});
+      int table_idx = table_mapping[d1];
+      tables[table_idx] = tables[table_idx].join(new_table, d1, d2);
+    } else {
+      // case 2: d1 and d2 are in different tables (impossible for now? just do select clause last)
+      assert(false);
+    }
+  }
+}
+
+
+std::vector<std::string> IntermediateResultsTable::getTableCol(const PqlDeclaration& d) {
+  assert(column_mapping.count(d) == 1);
+  int col_idx = column_mapping[d];
+
+  std::vector<std::string> output = {};
+  for (auto& row: table) {
+    output.push_back(row[col_idx]);
+  }
+  return output;
 }
--- Team30/Code30/src/spa/src/query_processing_system/evaluator/IntermediateResultsTable.h
+++ Team30/Code30/src/spa/src/query_processing_system/evaluator/IntermediateResultsTable.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "RelationalTable.h"
 #include "query_processing_system/common/ClauseResult.h"
 class IntermediateResultsTable {
   /**
@@ -8,29 +9,38 @@ class IntermediateResultsTable {
    */
  private:
   bool has_no_results = false;
+  std::vector<RelationalTable> tables;
+  std::unordered_map<PqlDeclaration, int, PqlDeclarationHash> table_mapping;
 
-  std::unordered_map<PqlDeclaration, std::vector<std::string>,
-                     PqlDeclarationHash>
-      value_map;
 
-  std::unordered_map<PqlDeclaration,
-                     std::unordered_set<PqlDeclaration, PqlDeclarationHash>,
-                     PqlDeclarationHash>
-      linked_declarations;
+  // one more data structure mapping pql to col_idx i think.
+  // DATA STRUCTURE NEEDS TO BE 2D LIST? table format, inner joiN?
+//  std::unordered_map<PqlDeclaration, std::vector<std::string>,
+//                     PqlDeclarationHash>
+//      value_map;
+
+//  std::unordered_map<PqlDeclaration,
+//                     std::unordered_set<PqlDeclaration, PqlDeclarationHash>,
+//                     PqlDeclarationHash>
+//      linked_declarations;
 
   void addBooleanClauseResult(bool result);
   void addSingleDeclaration(const PqlDeclaration& d,
-                            std::unique_ptr<std::vector<std::string>> values);
+                            const std::vector<std::string> &values);
   void addPairedDeclarations(const PqlDeclaration& d1, const PqlDeclaration& d2,
-                             std::unique_ptr<std::vector<std::string>> d1_values,
-                             std::unique_ptr<std::vector<std::string>> d2_values);
+                             const std::vector<std::string> & new_d1_values,
+                             const std::vector<std::string> & new_d2_values);
+
+  std::vector<std::string> getTableCol(const PqlDeclaration &d);
+
+
 
  public:
   IntermediateResultsTable();
 
-
   void addClauseResult(const ClauseResult& clause_result);
 
+  bool hasNoResults() const;
 
-  std::unique_ptr<std::vector<std::string>> getValuesGivenDeclaration(const PqlDeclaration &declaration);
+  std::vector<std::string> getValuesGivenDeclaration(const PqlDeclaration &declaration);
 };
