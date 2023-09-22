d9862d29ded4457113f4a27e7034ec6b5520f1b6
limweiliang <limweiliang4@gmail.com>
2023-09-22 22:58:53 +0800

    Fix bug

--- Team30/Code30/src/unit_testing/src/TestIntermediateResultsTable.cpp
+++ Team30/Code30/src/unit_testing/src/TestIntermediateResultsTable.cpp
@@ -1,24 +1,3 @@
 #include "catch.hpp"
 #include "query_processing_system/evaluator/IntermediateResultsTable.h"
 
-//TEST_CASE("Test getIndicesToKeep") {
-//  // Edge cases
-//  auto result = IntermediateResultsTable::getIndicesToKeep({}, {"blah","de"});
-//  REQUIRE(*result == std::vector<int>());
-//
-//  result = IntermediateResultsTable::getIndicesToKeep({"blah","de"}, {});
-//  REQUIRE(*result == std::vector<int>());
-//
-//  std::vector<std::string> arr = {"65","20","40", "40", "20","10"};
-//  std::vector<std::string> overlap_arr = {"20","20", "40", "10"};
-//
-//  result = IntermediateResultsTable::getIndicesToKeep(arr, overlap_arr);
-//  REQUIRE(*result == std::vector<int>({1,2,4,5}));
-//
-//  // try with longer strings
-//  arr = {"varX","varY","count", "idx", "idx", "idx"};
-//  overlap_arr = {"idx","idx", "count", "nonsense", "otherNonsense"};
-//
-//  result = IntermediateResultsTable::getIndicesToKeep(arr, overlap_arr);
-//  REQUIRE(*result == std::vector<int>({2,3,4}));
-//}
