acb0cfb15a4da190e55f7662c91b66db7ba52977
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2023-09-23 00:22:22 +0800

    add parsing of _ as special char token

--- Team30/Code30/src/spa/src/shared/tokenizer/Tokenizer.cpp
+++ Team30/Code30/src/spa/src/shared/tokenizer/Tokenizer.cpp
@@ -1,5 +1,7 @@
 #include "Tokenizer.h"
 
+#include <iostream>
+
 #include "token/EofToken.h"
 #include "token/IntegerToken.h"
 #include "token/SpecialCharToken.h"
@@ -48,7 +50,8 @@ std::shared_ptr<Token> Tokenizer::next() {
 
     else if (c == '{' || c == '}' || c == '(' || c == ')' || c == '+' ||
              c == '-' || c == '/' || c == '*' || c == '%' || c == ';' ||
-             c == ',' || c == '\"') {
+             c == ',' || c == '\"'|| c == '_') {
+      /* std::cout << "\ntokenizer1: " << c << "\n"; */
       return std::make_shared<SpecialCharToken>(std::string(1, c));
     }
 
