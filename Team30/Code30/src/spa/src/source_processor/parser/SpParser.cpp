82c7b6107c17800af5733bdda2f8af2a6ed1e6a7
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-09-26 12:14:22 +0800

    Push code back

--- Team30/Code30/src/spa/src/source_processor/parser/SpParser.cpp
+++ Team30/Code30/src/spa/src/source_processor/parser/SpParser.cpp
@@ -270,6 +270,9 @@ void SpParser::handleOperatorToken(
 
 void SpParser::handleLeftParenthesisToken(
     std::stack<std::shared_ptr<std::string>>& operatorStack, int& parenCount) {
+  if (AParser::isPeekTokenValue(SpParserConstant::RIGHT_PARENTHESIS)) {
+    throw std::invalid_argument("Empty parentheses");
+  }
   ++parenCount;
   operatorStack.push(std::make_shared<std::string>(getCurrTokenValue()));
 }
