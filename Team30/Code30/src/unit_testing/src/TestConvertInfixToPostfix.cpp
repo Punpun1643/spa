d100fe184478418e8576282cfab51377253a5d29
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-01 21:13:32 +0800

    Test: add negative test cases for invalid expression

--- Team30/Code30/src/unit_testing/src/TestConvertInfixToPostfix.cpp
+++ Team30/Code30/src/unit_testing/src/TestConvertInfixToPostfix.cpp
@@ -18,7 +18,9 @@ class TestableParser : public AParser {
 
 TEST_CASE("Test Convert infix expression to postfix expression",
           "[ConvertInfixToPostfix]") {
-  SECTION("Test infix expression i.e. v") {
+  SECTION(
+      "Test infix expression should correctly convert to postfix expression "
+      "i.e. v") {
     std::vector<std::shared_ptr<Token>> infixTokens;
 
     infixTokens.push_back(
@@ -36,7 +38,9 @@ TEST_CASE("Test Convert infix expression to postfix expression",
     REQUIRE(*first == "v");
   }
 
-  SECTION("Test infix expression i.e. x + y") {
+  SECTION(
+      "Test infix expression should correctly convert to postfix expression "
+      "i.e. x + y") {
     std::vector<std::shared_ptr<Token>> infixTokens;
 
     infixTokens.push_back(
@@ -64,7 +68,9 @@ TEST_CASE("Test Convert infix expression to postfix expression",
     REQUIRE(*third == "+");
   }
 
-  SECTION("Test infix expression i.e. v + x * y") {
+  SECTION(
+      "Test infix expression should correctly convert to postfix expression "
+      "i.e. v + x * y") {
     std::vector<std::shared_ptr<Token>> infixTokens;
 
     infixTokens.push_back(
@@ -102,7 +108,9 @@ TEST_CASE("Test Convert infix expression to postfix expression",
     REQUIRE(*fifth == "+");
   }
 
-  SECTION("Test infix expression i.e. x * y + z * t") {
+  SECTION(
+      "Test infix expression should correctly convert to postfix expression "
+      "i.e. x * y + z * t") {
     std::vector<std::shared_ptr<Token>> infixTokens;
 
     infixTokens.push_back(
@@ -150,158 +158,217 @@ TEST_CASE("Test Convert infix expression to postfix expression",
     REQUIRE(*seventh == "+");
   }
 
-  SECTION("Test infix expression i.e. x / y + z * t") {
-        std::vector<std::shared_ptr<Token>> infixTokens;
+  SECTION(
+      "Test infix expression should correctly convert to postfix expression "
+      "i.e. x / y + z * t") {
+    std::vector<std::shared_ptr<Token>> infixTokens;
 
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("x")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("/")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("y")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("+")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("z")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("*")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("t")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("x")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("/")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("y")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("+")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("z")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("*")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("t")));
 
-        std::shared_ptr<TestableParser> parser =
-                std::make_shared<TestableParser>(infixTokens);
+    std::shared_ptr<TestableParser> parser =
+        std::make_shared<TestableParser>(infixTokens);
 
-        std::queue<std::shared_ptr<std::string>> postFixQueue =
-                parser->ConvertInfixToPostfix(infixTokens);
+    std::queue<std::shared_ptr<std::string>> postFixQueue =
+        parser->ConvertInfixToPostfix(infixTokens);
 
-        std::shared_ptr<std::string> first = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> second = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> third = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> fourth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> fifth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> sixth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> seventh = postFixQueue.front();
-        postFixQueue.pop();
+    std::shared_ptr<std::string> first = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> second = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> third = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> fourth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> fifth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> sixth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> seventh = postFixQueue.front();
+    postFixQueue.pop();
 
-        REQUIRE(*first == "x");
-        REQUIRE(*second == "y");
-        REQUIRE(*third == "/");
-        REQUIRE(*fourth == "z");
-        REQUIRE(*fifth == "t");
-        REQUIRE(*sixth == "*");
-        REQUIRE(*seventh == "+");
+    REQUIRE(*first == "x");
+    REQUIRE(*second == "y");
+    REQUIRE(*third == "/");
+    REQUIRE(*fourth == "z");
+    REQUIRE(*fifth == "t");
+    REQUIRE(*sixth == "*");
+    REQUIRE(*seventh == "+");
   }
 
-  SECTION("Test infix expression i.e. x * y / z * t") {
-        std::vector<std::shared_ptr<Token>> infixTokens;
+  SECTION(
+      "Test infix expression should correctly convert to postfix expression "
+      "i.e. x * y / z * t") {
+    std::vector<std::shared_ptr<Token>> infixTokens;
 
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("x")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("*")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("y")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("/")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("z")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("*")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("t")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("x")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("*")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("y")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("/")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("z")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("*")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("t")));
 
-        std::shared_ptr<TestableParser> parser =
-                std::make_shared<TestableParser>(infixTokens);
+    std::shared_ptr<TestableParser> parser =
+        std::make_shared<TestableParser>(infixTokens);
 
-        std::queue<std::shared_ptr<std::string>> postFixQueue =
-                parser->ConvertInfixToPostfix(infixTokens);
+    std::queue<std::shared_ptr<std::string>> postFixQueue =
+        parser->ConvertInfixToPostfix(infixTokens);
 
-        std::shared_ptr<std::string> first = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> second = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> third = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> fourth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> fifth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> sixth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> seventh = postFixQueue.front();
-        postFixQueue.pop();
+    std::shared_ptr<std::string> first = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> second = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> third = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> fourth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> fifth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> sixth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> seventh = postFixQueue.front();
+    postFixQueue.pop();
 
-        REQUIRE(*first == "x");
-        REQUIRE(*second == "y");
-        REQUIRE(*third == "*");
-        REQUIRE(*fourth == "z");
-        REQUIRE(*fifth == "/");
-        REQUIRE(*sixth == "t");
-        REQUIRE(*seventh == "*");
+    REQUIRE(*first == "x");
+    REQUIRE(*second == "y");
+    REQUIRE(*third == "*");
+    REQUIRE(*fourth == "z");
+    REQUIRE(*fifth == "/");
+    REQUIRE(*sixth == "t");
+    REQUIRE(*seventh == "*");
   }
 
-  SECTION("Test infix expression i.e. (((v) + ((x) * (y))) + ((z) * (t)))") {
-        std::vector<std::shared_ptr<Token>> infixTokens;
+  SECTION(
+      "Test infix expression should correctly convert to postfix expression "
+      "i.e. (((v) + ((x) * (y))) + ((z) * (t)))") {
+    std::vector<std::shared_ptr<Token>> infixTokens;
 
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("v")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("+")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("x")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("*")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("y")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("+")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("z")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("*")));
-        infixTokens.push_back(
-                std::static_pointer_cast<Token>(std::make_shared<WordToken>("t")));
-        infixTokens.push_back(std::static_pointer_cast<Token>(
-                std::make_shared<SpecialCharToken>("+")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("v")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("+")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("x")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("*")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("y")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("+")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("z")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("*")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("t")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("+")));
 
-        std::shared_ptr<TestableParser> parser =
-                std::make_shared<TestableParser>(infixTokens);
+    std::shared_ptr<TestableParser> parser =
+        std::make_shared<TestableParser>(infixTokens);
 
-        std::queue<std::shared_ptr<std::string>> postFixQueue =
-                parser->ConvertInfixToPostfix(infixTokens);
+    std::queue<std::shared_ptr<std::string>> postFixQueue =
+        parser->ConvertInfixToPostfix(infixTokens);
 
-        std::shared_ptr<std::string> first = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> second = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> third = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> fourth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> fifth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> sixth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> seventh = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> eighth = postFixQueue.front();
-        postFixQueue.pop();
-        std::shared_ptr<std::string> ninth = postFixQueue.front();
+    std::shared_ptr<std::string> first = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> second = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> third = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> fourth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> fifth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> sixth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> seventh = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> eighth = postFixQueue.front();
+    postFixQueue.pop();
+    std::shared_ptr<std::string> ninth = postFixQueue.front();
 
-        REQUIRE(*first == "v");
-        REQUIRE(*second == "x");
-        REQUIRE(*third == "y");
-        REQUIRE(*fourth == "*");
-        REQUIRE(*fifth == "+");
-        REQUIRE(*sixth == "z");
-        REQUIRE(*seventh == "t");
-        REQUIRE(*eighth == "*");
-        REQUIRE(*ninth == "+");
+    REQUIRE(*first == "v");
+    REQUIRE(*second == "x");
+    REQUIRE(*third == "y");
+    REQUIRE(*fourth == "*");
+    REQUIRE(*fifth == "+");
+    REQUIRE(*sixth == "z");
+    REQUIRE(*seventh == "t");
+    REQUIRE(*eighth == "*");
+    REQUIRE(*ninth == "+");
+  }
+
+  SECTION("Test invalid infix expression should throw error i.e. (x + y * z") {
+    std::vector<std::shared_ptr<Token>> infixTokens;
+
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("(")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("x")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("+")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("y")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("*")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("z")));
+
+    std::shared_ptr<TestableParser> parser =
+        std::make_shared<TestableParser>(infixTokens);
+
+    REQUIRE_THROWS_AS(parser->ConvertInfixToPostfix(infixTokens),
+                      std::invalid_argument);
+  }
+
+  SECTION(
+      "Test invalid infix expression should throw error i.e. ((x + y))) * z") {
+    std::vector<std::shared_ptr<Token>> infixTokens;
+
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("(")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("(")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("x")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("+")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("y")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>(")")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>(")")));
+    infixTokens.push_back(std::static_pointer_cast<Token>(
+        std::make_shared<SpecialCharToken>("*")));
+    infixTokens.push_back(
+        std::static_pointer_cast<Token>(std::make_shared<WordToken>("z")));
+
+    std::shared_ptr<TestableParser> parser =
+        std::make_shared<TestableParser>(infixTokens);
+
+    REQUIRE_THROWS_AS(parser->ConvertInfixToPostfix(infixTokens),
+                      std::invalid_argument);
   }
 }
