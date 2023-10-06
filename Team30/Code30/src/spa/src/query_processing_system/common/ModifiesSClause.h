171168b204b00bab4111ee6011bc3f23939137ad
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-06 21:34:40 +0800

    Fix: merge conflict

--- Team30/Code30/src/spa/src/query_processing_system/common/ModifiesSClause.h
+++ Team30/Code30/src/spa/src/query_processing_system/common/ModifiesSClause.h
@@ -4,13 +4,14 @@
 
 #include "EntRef.h"
 #include "EntityType.h"
+#include "StmtRef.h"
 #include "UsesOrModifiesClause.h"
 
-class ModifiesPClause : public UsesOrModifiesClause {
+class ModifiesSClause : public UsesOrModifiesClause {
  private:
   static std::vector<EntityType> const VALID_DECL_TYPES_FOR_ARG_1;
   static std::string const INVALID_ARG_1_DECL_TYPE_MSG;
 
  public:
-  ModifiesPClause(std::unique_ptr<EntRef> arg1, std::unique_ptr<EntRef> arg2);
+  ModifiesSClause(std::unique_ptr<StmtRef> arg1, std::unique_ptr<EntRef> arg2);
 };
