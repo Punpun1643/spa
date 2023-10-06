10a35af1fa3cad4bf06764bfbb9144cee67f0386
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-06 20:55:04 +0800

    Create UsesP and Modifies P Clauses

---
+++ Team30/Code30/src/spa/src/query_processing_system/common/ModifiesPClause.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <memory>
+#include <string>
+
+#include "EntRef.h"
+#include "EntityType.h"
+#include "UsesOrModifiesClause.h"
+
+class ModifiesPClause : public UsesOrModifiesClause {
+ private:
+  static const std::vector<EntityType> VALID_DECL_TYPES_FOR_ARG_1;
+  static const std::string INVALID_ARG_1_DECL_TYPE_MSG;
+
+ public:
+  ModifiesPClause(std::unique_ptr<EntRef> arg1, std::unique_ptr<EntRef> arg2);
+};
---
+++ Team30/Code30/src/spa/src/query_processing_system/common/UsesPClause.cpp
@@ -0,0 +1,12 @@
+#include "UsesPClause.h"
+
+#include <string>
+
+UsesPClause::UsesPClause(std::unique_ptr<EntRef> arg1, std::unique_ptr<EntRef> arg2)
+    :UsesOrModifiesClause(std::move(arg1), std::move(arg2), RelationType::USES_P, VALID_DECL_TYPES_FOR_ARG_1, INVALID_ARG_1_DECL_TYPE_MSG) {}
+
+const std::vector<EntityType> UsesPClause::VALID_DECL_TYPES_FOR_ARG_1 =
+    {EntityType::PROCEDURE};
+
+const std::string UsesPClause::INVALID_ARG_1_DECL_TYPE_MSG =
+    "If the 1st arg of UsesP is a declaration, it must have the procedure type.";
