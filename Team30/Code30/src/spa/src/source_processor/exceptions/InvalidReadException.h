529a65e40caa14b5b515f307c9b1768a49c2e2e8
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-07 20:19:30 +0800

    Feat: declare invalid read exception

---
+++ Team30/Code30/src/spa/src/source_processor/exceptions/InvalidReadException.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+class InvalidReadException : public std::invalid_argument {
+  /**
+   * Thrown when there is an invalid read stmt.
+   */
+ public:
+  explicit InvalidReadException(std::string const& what_arg = "Invalid read statement!");
+};
