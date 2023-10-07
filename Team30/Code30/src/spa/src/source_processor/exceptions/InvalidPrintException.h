46513ec67651cac706685ac374e2e438aaac864d
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-07 20:12:32 +0800

    Feat: create print exception

---
+++ Team30/Code30/src/spa/src/source_processor/exceptions/InvalidPrintException.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+class InvalidPrintException : public std::invalid_argument {
+  /**
+   * Thrown when a there is an invalid print stmt.
+   */
+ public:
+  explicit InvalidPrintException(std::string const& what_arg = "Invalid print statement!");
+};
