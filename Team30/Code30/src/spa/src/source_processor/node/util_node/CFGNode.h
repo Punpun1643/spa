24e3faca5a7268cc8729b53afe48f179b4806f52
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-14 16:11:59 +0800

    Test: test has immediate path

--- Team30/Code30/src/spa/src/source_processor/node/util_node/CFGNode.h
+++ Team30/Code30/src/spa/src/source_processor/node/util_node/CFGNode.h
@@ -1,21 +1,27 @@
 #pragma once
 
-#include <query_processing_system/common/EntityType.h>
-#include <source_processor/node/stmt_node/StmtNode.h>
-
 #include <memory>
 #include <vector>
+
+#include "../../../query_processing_system/common/EntityType.h"
+#include "../util_node/CFGNode.h"
+#include  "../stmt_node/StmtNode.h"
+#include "../stmt_node/StmtType.h"
 
 class CFGNode {
  public:
   CFGNode(std::shared_ptr<StmtNode> node);
 
   std::vector<std::shared_ptr<CFGNode>> getOutgoingNodes();
+
   std::vector<std::shared_ptr<CFGNode>> getIncomingNodes();
+
   std::shared_ptr<StmtNode> getNode();
+
   StmtType getNodeType();
 
   void addOutgoingNode(std::shared_ptr<CFGNode>);
+
   void addIncomingNode(std::shared_ptr<CFGNode>);
 
   static bool HasImmediatePath(std::shared_ptr<CFGNode> startNode, std::shared_ptr<CFGNode> endNode);
