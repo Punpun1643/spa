bc7f161420b623923bbab3850c90cb0f9a1beb07
limweiliang <limweiliang4@gmail.com>
2023-10-14 17:53:28 +0800

    Make AttrRef data public

--- Team30/Code30/src/spa/src/query_processing_system/common/AttrRef.h
+++ Team30/Code30/src/spa/src/query_processing_system/common/AttrRef.h
@@ -14,14 +14,15 @@ class AttrRef {
  private:
   PqlDeclaration decl;
   AttrType attr_type;
-  static const std::unordered_map<EntityType, AttrType> DEFAULT_ATTR_TYPE;
-  static const std::unordered_map<EntityType, AttrType> ATTR_TYPE_ALIASES;
   static const std::unordered_map<AttrType, AttrRefOutputType> OUTPUT_TYPE_MAPPING;
 
   void CheckTypeCombinationValidity() const;
   AttrType GetDefaultAttrType() const;
 
  public:
+  static const std::unordered_map<EntityType, AttrType> DEFAULT_ATTR_TYPE;
+  static const std::unordered_map<EntityType, AttrType> ATTR_TYPE_ALIASES;
+
   explicit AttrRef(PqlDeclaration decl);
 
   AttrRef(PqlDeclaration decl, AttrType attr_type);
