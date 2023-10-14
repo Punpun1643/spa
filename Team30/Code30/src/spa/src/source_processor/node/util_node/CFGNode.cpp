ad153c2885db411c1c75f87ac263e4e055f8ae5e
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-14 15:16:00 +0800

    Feat: implement next*

--- Team30/Code30/src/spa/src/source_processor/node/util_node/CFGNode.cpp
+++ Team30/Code30/src/spa/src/source_processor/node/util_node/CFGNode.cpp
@@ -1,5 +1,8 @@
 #include "CFGNode.h"
 
+#include <queue>
+#include <unordered_set>
+
 CFGNode::CFGNode(std::shared_ptr<StmtNode> node) : node(node) {}
 
 std::vector<std::shared_ptr<CFGNode>> CFGNode::getOutgoingNodes() {
@@ -24,7 +27,36 @@ void CFGNode::addIncomingNode(std::shared_ptr<CFGNode> newNode) {
 
 bool CFGNode::HasPath(std::shared_ptr<CFGNode> startNode,
                       std::shared_ptr<CFGNode> endNode) {
-  return true;
+  //  if (startNode == endNode) {
+  //    return true;
+  //  }
+
+  // visited nodes
+  std::unordered_set<std::shared_ptr<CFGNode>> visitedNodes;
+  // nodes to visit
+  std::queue<std::shared_ptr<CFGNode>> nodesToVisit;
+
+  nodesToVisit.push(startNode);
+
+  while (!nodesToVisit.empty()) {
+    std::shared_ptr<CFGNode> currNode = nodesToVisit.front();
+    nodesToVisit.pop();
+    visitedNodes.insert(currNode);
+
+    if (currNode == endNode) {
+      return true;
+    }
+
+    std::vector<std::shared_ptr<CFGNode>> outgoingNodes =
+        currNode->getOutgoingNodes();
+
+    for (std::shared_ptr<CFGNode> outgoingNode : outgoingNodes) {
+      if (visitedNodes.find(outgoingNode) == visitedNodes.end()) {
+        nodesToVisit.push(outgoingNode);
+      }
+    }
+  }
+  return false;
 }
 
 bool CFGNode::HasAffectPath(std::shared_ptr<CFGNode> startNode,
