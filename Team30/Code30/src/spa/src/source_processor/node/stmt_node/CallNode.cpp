b26028c97f92e3732ddb1d2e52ba8eacaa371ecc
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-22 15:59:32 +0800

    Refactor: call node

--- Team30/Code30/src/spa/src/source_processor/node/stmt_node/CallNode.cpp
+++ Team30/Code30/src/spa/src/source_processor/node/stmt_node/CallNode.cpp
@@ -2,15 +2,15 @@
 
 #include "../../design_extractor/IDesignExtractor.h"
 
-CallNode::CallNode(int stmtIndex, StmtType stmtType, std::string procName)
-    : StmtNode(stmtIndex, StmtType::CALL_STMT), procName(procName) {}
+CallNode::CallNode(int stmt_index, StmtType stmt_type, std::string proc_name)
+    : StmtNode(stmt_index, StmtType::CALL_STMT), proc_name(proc_name) {}
 
-void CallNode::Accept(IDesignExtractor& designExtractor) {
-  auto thisCallNodePtr =
+void CallNode::Accept(IDesignExtractor& design_extractor) {
+  auto this_call_node_ptr =
       std::dynamic_pointer_cast<CallNode>(shared_from_this());
-  designExtractor.ExtractFromCall(thisCallNodePtr);
+  design_extractor.ExtractFromCall(this_call_node_ptr);
 }
 
 std::string const& CallNode::GetProcName() const {
-  return procName;
+  return proc_name;
 }
