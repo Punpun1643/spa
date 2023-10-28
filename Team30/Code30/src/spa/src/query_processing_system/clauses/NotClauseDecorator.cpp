f463c9dc9cbb8087db2c886b64d458cf7927bc8d
limweiliang <limweiliang4@gmail.com>
2023-10-28 15:08:50 +0800

    Reformat code

--- Team30/Code30/src/spa/src/query_processing_system/clauses/NotClauseDecorator.cpp
+++ Team30/Code30/src/spa/src/query_processing_system/clauses/NotClauseDecorator.cpp
@@ -5,14 +5,17 @@
 #include "SelectAllClause.h"
 
 NotClauseDecorator::NotClauseDecorator(std::unique_ptr<Clause> clause_to_negate)
-  : clause_to_negate(std::move(clause_to_negate)) {}
+    : clause_to_negate(std::move(clause_to_negate)) {}
 
-std::vector<std::string> NotClauseDecorator::NegateSingleDeclValues(PKBQPSInterface& pkb, PqlDeclaration const& decl, std::unordered_set<std::string> const& value_set) {
+std::vector<std::string> NotClauseDecorator::NegateSingleDeclValues(
+    PKBQPSInterface& pkb, PqlDeclaration const& decl,
+    std::unordered_set<std::string> const& value_set) {
   auto select_all_clause = SelectAllClause(decl);
-  std::vector<std::string> all_values = select_all_clause.Evaluate(pkb)->GetValues(decl);
+  std::vector<std::string> all_values =
+      select_all_clause.Evaluate(pkb)->GetValues(decl);
 
   std::vector<std::string> complement_values;
-  for (auto& value: all_values) {
+  for (auto& value : all_values) {
     if (value_set.count(value) == 0) {
       complement_values.push_back(value);
     }
@@ -20,19 +23,23 @@ std::vector<std::string> NotClauseDecorator::NegateSingleDeclValues(PKBQPSInterf
   return complement_values;
 }
 
-std::vector<std::pair<std::string, std::string>> NotClauseDecorator::NegateDoubleDeclValues(PKBQPSInterface& pkb,
-                                                                    PqlDeclaration const& decl_1,
-                                                                    PqlDeclaration const& decl_2,
-                                                                    std::unordered_set<std::pair<std::string, std::string>, PairHash> const& value_set) {
+std::vector<std::pair<std::string, std::string>>
+NotClauseDecorator::NegateDoubleDeclValues(
+    PKBQPSInterface& pkb, PqlDeclaration const& decl_1,
+    PqlDeclaration const& decl_2,
+    std::unordered_set<std::pair<std::string, std::string>, PairHash> const&
+        value_set) {
   auto select_all_clause_1 = SelectAllClause(decl_1);
   auto select_all_clause_2 = SelectAllClause(decl_2);
 
-  std::vector<std::string> all_values_1 = select_all_clause_1.Evaluate(pkb)->GetValues(decl_1);
-  std::vector<std::string> all_values_2 = select_all_clause_2.Evaluate(pkb)->GetValues(decl_2);
+  std::vector<std::string> all_values_1 =
+      select_all_clause_1.Evaluate(pkb)->GetValues(decl_1);
+  std::vector<std::string> all_values_2 =
+      select_all_clause_2.Evaluate(pkb)->GetValues(decl_2);
 
   std::vector<std::pair<std::string, std::string>> complement_pairs;
-  for (auto& value_1: all_values_1) {
-    for (auto& value_2: all_values_2) {
+  for (auto& value_1 : all_values_1) {
+    for (auto& value_2 : all_values_2) {
       auto pair = std::make_pair(value_1, value_2);
       if (value_set.count(pair) == 0) {
         complement_pairs.push_back(pair);
@@ -42,14 +49,15 @@ std::vector<std::pair<std::string, std::string>> NotClauseDecorator::NegateDoubl
   return complement_pairs;
 }
 
-std::unique_ptr<ClauseResult> NotClauseDecorator::Evaluate(PKBQPSInterface& pkb) {
+std::unique_ptr<ClauseResult> NotClauseDecorator::Evaluate(
+    PKBQPSInterface& pkb) {
   auto results = clause_to_negate->Evaluate(pkb);
   switch (results->GetNumDeclarations()) {
-    case(0): {
+    case (0): {
       bool value = results->GetBooleanClauseValue();
       return std::make_unique<ClauseResult>(!value);
     }
-    case(1): {
+    case (1): {
       PqlDeclaration decl = results->GetDeclarations()[0];
       std::vector<std::string> values = results->GetValues(decl);
 
@@ -59,25 +67,29 @@ std::unique_ptr<ClauseResult> NotClauseDecorator::Evaluate(PKBQPSInterface& pkb)
       auto complement_values = NegateSingleDeclValues(pkb, decl, value_set);
       return std::make_unique<ClauseResult>(decl, complement_values);
     }
-    case(2): {
+    case (2): {
       PqlDeclaration decl_1 = results->GetDeclarations()[0];
       PqlDeclaration decl_2 = results->GetDeclarations()[1];
 
-      // TODO(@limweiliang): Add helper method in ClauseResult to return appropriate data structure directly?
+      // TODO(@limweiliang): Add helper method in ClauseResult to return
+      // appropriate data structure directly?
       std::vector<std::string> value_vec_1 = results->GetValues(decl_1);
       std::vector<std::string> value_vec_2 = results->GetValues(decl_2);
       if (value_vec_1.size() != value_vec_2.size()) {
         throw std::logic_error("Value vectors have different lengths.");
       }
 
-      std::unordered_set<std::pair<std::string, std::string>, PairHash> value_set;
+      std::unordered_set<std::pair<std::string, std::string>, PairHash>
+          value_set;
       for (int i = 0; i < value_vec_1.size(); i++) {
         value_set.emplace(value_vec_1[i], value_vec_2[i]);
       }
-      auto complement_values = NegateDoubleDeclValues(pkb, decl_1, decl_2, value_set);
+      auto complement_values =
+          NegateDoubleDeclValues(pkb, decl_1, decl_2, value_set);
       return std::make_unique<ClauseResult>(decl_1, decl_2, complement_values);
     }
     default:
-      throw std::logic_error("ClauseResult has an unexpected number of declarations.");
+      throw std::logic_error(
+          "ClauseResult has an unexpected number of declarations.");
   }
 }
--- Team30/Code30/src/spa/src/query_processing_system/clauses/NotClauseDecorator.h
+++ Team30/Code30/src/spa/src/query_processing_system/clauses/NotClauseDecorator.h
@@ -4,12 +4,17 @@
 #include "Clause.h"
 #include "program_knowledge_base/utils/PairHash.h"
 
-class NotClauseDecorator: public Clause {
+class NotClauseDecorator : public Clause {
  private:
   std::unique_ptr<Clause> clause_to_negate;
-  static std::vector<std::string> NegateSingleDeclValues(PKBQPSInterface& pkb, PqlDeclaration const& decl, std::unordered_set<std::string> const& value_set);
-  static std::vector<std::pair<std::string, std::string>> NegateDoubleDeclValues(PKBQPSInterface& pkb, PqlDeclaration const& decl_1, PqlDeclaration const& decl_2,
-                                                  std::unordered_set<std::pair<std::string, std::string>, PairHash> const& value_set);
+  static std::vector<std::string> NegateSingleDeclValues(
+      PKBQPSInterface& pkb, PqlDeclaration const& decl,
+      std::unordered_set<std::string> const& value_set);
+  static std::vector<std::pair<std::string, std::string>>
+  NegateDoubleDeclValues(PKBQPSInterface& pkb, PqlDeclaration const& decl_1,
+                         PqlDeclaration const& decl_2,
+                         std::unordered_set<std::pair<std::string, std::string>,
+                                            PairHash> const& value_set);
 
  public:
   explicit NotClauseDecorator(std::unique_ptr<Clause> clause_to_negate);
