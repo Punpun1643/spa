672e425f1d84e25e2588959e302b62b3b7a405e9
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-29 10:37:18 +0800

    Refactor: migrate CFGNode static methods to graph class

---
+++ Team30/Code30/src/spa/src/source_processor/util/GraphRelationTraverser.cpp
@@ -0,0 +1,6 @@
+#include "GraphRelationTraverser.h"
+
+bool GraphRelationTraverser::HasImmediatePath(
+    std::shared_ptr<CFGNode> start_node, std::shared_ptr<CFGNode> end_node) {
+  return true;
+}
---
+++ Team30/Code30/src/spa/src/source_processor/util/GraphRelationTraverser.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_set>
+
+#include "../node/util_node/CFGNode.h"
+
+class GraphRelationTraverser {
+ public:
+  static bool HasImmediatePath(std::shared_ptr<CFGNode> start_node,
+                               std::shared_ptr<CFGNode> end_node);
+
+  static bool HasPath(std::shared_ptr<CFGNode> start_node,
+                      std::shared_ptr<CFGNode> end_node);
+
+  static bool HasAffectsPath(std::shared_ptr<CFGNode> start_node,
+                             std::shared_ptr<CFGNode> end_node);
+
+  static bool IsAssignOrReadOutgoingNode(std::shared_ptr<CFGNode> node);
+
+  static bool IsCallOutgoingNode(std::shared_ptr<CFGNode> node);
+
+  static bool HandleAssignOrReadOutgoingNode(
+      std::shared_ptr<CFGNode> outgoing_node,
+      std::string const& var_modified_in_start_node);
+
+  static bool HandleCallOutgoingNode(
+      std::shared_ptr<CFGNode> outgoing_node,
+      std::string const& var_modified_in_start_node);
+
+  static bool ValidateStartAndEndNodes(std::shared_ptr<CFGNode> start_node,
+                                       std::shared_ptr<CFGNode> end_node);
+
+  static std::string GetVarModifiedInStartNode(
+      std::shared_ptr<CFGNode> start_node);
+
+  static std::unordered_set<std::string> GetVarUsedInEndNode(
+      std::shared_ptr<CFGNode> end_node);
+
+  static bool ValidatePossibleAffectsRelationship(
+      std::string var_modified_in_start_node,
+      std::unordered_set<std::string> var_used_in_end_node);
+};
