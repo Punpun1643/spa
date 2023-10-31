159f7c8e94e1559c54e56b0a7fffc5628609c825
Lee Kang Wei <65090312+lkwlkww@users.noreply.github.com>
2023-10-31 11:22:10 +0800

    fix error

--- Team30/Code30/src/spa/src/query_processing_system/expression/PatternAssignExpression.h
+++ Team30/Code30/src/spa/src/query_processing_system/expression/PatternAssignExpression.h
@@ -12,7 +12,7 @@ class PatternAssignExpression : public PatternExpression {
  public:
   PatternAssignExpression(std::string synonym, std::string arg1,
                           std::string arg2, MatchType match_type,
-                          std::shared_ptr<TreeNode> rhs_expr_tree)
+                          std::shared_ptr<TreeNode> rhs_expr_tree);
 
   void acceptInterpreter(QueryInterpreter& interpreter) override;
   MatchType GetMatchType();
