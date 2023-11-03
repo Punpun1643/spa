313e6827488acd3868cb0ca81153a09d474b267a
limweiliang <limweiliang4@gmail.com>
2023-11-03 22:40:55 +0800

    Add std::move to ClauseResult constructor calls

--- Team30/Code30/src/spa/src/query_processing_system/clauses/WithClause.cpp
+++ Team30/Code30/src/spa/src/query_processing_system/clauses/WithClause.cpp
@@ -48,7 +48,7 @@ std::unique_ptr<ClauseResult> WithClause::EvaluateValueAttrRef(
     PKBQPSInterface& pkb, std::string const& value, AttrRef const& attr_ref) {
   std::vector result = pkb.GetEntitiesMatchingAttrValue(
       attr_ref.GetEntityType(), attr_ref.GetAttrType(), value);
-  return std::make_unique<ClauseResult>(attr_ref.GetDecl(), result);
+  return std::make_unique<ClauseResult>(attr_ref.GetDecl(), std::move(result));
 }
 
 std::unique_ptr<ClauseResult> WithClause::EvaluateIntAttrRef(
@@ -82,7 +82,7 @@ std::unique_ptr<ClauseResult> WithClause::EvaluateRefRef(PKBQPSInterface& pkb) {
       attr_ref_1.GetEntityType(), attr_ref_1.GetAttrType(),
       attr_ref_2.GetEntityType(), attr_ref_2.GetAttrType());
   return std::make_unique<ClauseResult>(attr_ref_1.GetDecl(),
-                                        attr_ref_2.GetDecl(), matching);
+                                        attr_ref_2.GetDecl(), std::move(matching));
 }
 
 std::unique_ptr<ClauseResult> WithClause::Evaluate(PKBQPSInterface& pkb) {
