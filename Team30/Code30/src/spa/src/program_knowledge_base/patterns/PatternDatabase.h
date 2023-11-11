cbd3dbe9da3ea8a27edfcc6670e9fbbf1f106c8d
tyanhan <tayyanhanw@gmail.com>
2023-11-11 16:35:30 +0800

    Fix violations

--- Team30/Code30/src/spa/src/program_knowledge_base/patterns/PatternDatabase.h
+++ Team30/Code30/src/spa/src/program_knowledge_base/patterns/PatternDatabase.h
@@ -13,6 +13,7 @@
 
 class PatternDatabase {
  private:
+  // ASSIGN STATEMENTS
   // {statement_number, (lhs, rhs)}
   std::unordered_map<std::string,
                      std::pair<std::string, std::shared_ptr<TreeNode>>>
@@ -21,6 +22,7 @@ class PatternDatabase {
   std::unordered_map<std::string, std::unordered_set<std::string>>
       lhs_assignments = {};
 
+  // IF/WHILE STATEMENTS
   // {stmt# : vars}
   std::unordered_map<
       EntityType,
@@ -31,6 +33,20 @@ class PatternDatabase {
       EntityType,
       std::unordered_map<std::string, std::unordered_set<std::string>>>
       inv_cond_var_patterns;
+
+  // helper functions
+  std::unordered_set<std::string> GetMatchingAssignStringPartial(
+      std::string const& lhs_value, std::shared_ptr<TreeNode> const& rhs_expr);
+  std::unordered_set<std::string> GetMatchingAssignStringExact(
+      std::string const& lhs_value, std::shared_ptr<TreeNode> const& rhs_expr);
+  std::unordered_set<std::string> GetMatchingAssignWildPartial(
+      std::shared_ptr<TreeNode> const& rhs_expr);
+  std::unordered_set<std::string> GetMatchingAssignWildExact(
+      std::shared_ptr<TreeNode> const& rhs_expr);
+  std::vector<std::pair<std::string, std::string>>
+  GetMatchingAssignSynonymPartial(std::shared_ptr<TreeNode> const& rhs_expr);
+  std::vector<std::pair<std::string, std::string>>
+  GetMatchingAssignSynonymExact(std::shared_ptr<TreeNode> const& rhs_expr);
 
  public:
   PatternDatabase();
