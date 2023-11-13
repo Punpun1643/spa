e8b895912fc13b0420b0b1c3fa5528c3a4e1816e
limweiliang <limweiliang4@gmail.com>
2023-11-13 10:30:48 +0800

    Make code quality changes to pkb utils

--- Team30/Code30/src/spa/src/program_knowledge_base/utils/Node.cpp
+++ Team30/Code30/src/spa/src/program_knowledge_base/utils/Node.cpp
@@ -1,19 +1,21 @@
 #include "Node.h"
 
-Node::Node(std::string const& value) : value(value) {}
+#include <utility>
 
-void Node::InsertNext(std::shared_ptr<Node> node) {
+Node::Node(std::string  value) : value(std::move(value)) {}
+
+void Node::InsertNext(const std::shared_ptr<Node>& node) {
   next.insert(node);
 }
 
-void Node::InsertPrev(std::shared_ptr<Node> node) {
+void Node::InsertPrev(const std::shared_ptr<Node>& node) {
   prev.insert(node);
 }
 
-bool Node::IsNextEmpty() {
+bool Node::IsNextEmpty() const {
   return next.empty();
 }
 
-bool Node::IsPrevEmpty() {
+bool Node::IsPrevEmpty() const {
   return prev.empty();
 }
